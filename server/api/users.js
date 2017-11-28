de8d36ffd33f785274af73dfb6802c3ba56981c4
xijing <xijing@rongzi.com>
2017-11-28 19:26:38 +0800

    1

--- server/api/users.js
+++ server/api/users.js
@@ -14,14 +14,14 @@ let users = [
 /* GET users listing. */
 router.get('/users', async function (req, res, next) {
   // let { data: result } = await axios.get('http://lpapi.rongzi.com/api/Account/IsLogin')
-  let newUsers = users.concat()
+  // let newUsers = users.concat()
   // if (result.Content) {
   //   newUsers.push({ name: 'Loduvic' })
   // } else {
   //   newUsers.push({ name: 'Alexis' })
   // }
   //logger('applog').debug(newUsers)
-  res.json(newUsers)
+  res.json(users)
 })
 
 /* GET user by ID. */
