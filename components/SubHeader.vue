d20d4660209625538730a868fdc4b7667970d70d
xijing <xijing@rongzi.com>
2018-01-03 18:22:08 +0800

    1

--- components/SubHeader.vue
+++ components/SubHeader.vue
@@ -2,6 +2,7 @@
   <header>
     <span class="sub-title">前端入门</span> 
     <span> by 席 璟</span>
+    <a class="download" target="_black" href="https://deltafx35c.com/resources/lesson-demos.zip">下载课件源码</a>
   </header>
 </template>
 <style scoped lang="scss">
@@ -18,6 +19,10 @@
     .sub-title{
       color:#fff;
       margin-right:30px;
+    }
+    .download{
+     margin-left:80px;
+     color:red; 
     }
   }
 </style>
